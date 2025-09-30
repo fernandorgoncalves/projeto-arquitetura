2e3466197c1b272a0d18a56816c40d563a240bc6
Fernando <fernandorgoncalves@hotmail.com>
2025-09-30 00:27:13 -0300

    Adicionando formulario

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,46 @@
-
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'
-import Home from './pages/Home'
-import About from './pages/About'
-import Projects from './pages/Projects'
-import Contact from './pages/Contact'
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
 
 //UTILS
-import ScrollToTop from './utils/ScrollToTop'
+import ScrollToTop from "./utils/ScrollToTop";
 
 function App() {
-
   return (
     <Router>
-      <ScrollToTop/>
-     <Routes>
-        <Route path ="/" element={<Home />}></Route>
-        <Route path ="/about" element={<><About /><Link to={"/"}>Navegar</Link></>}></Route>
-        <Route path ="/projects" element={<><Projects /></>}></Route>
-        <Route path ="/contacts" element={<><Contact /></>}></Route>
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route
+          path="/about"
+          element={
+            <>
+              <About />
+              <Link to={"/"}>Navegar</Link>
+            </>
+          }
+        ></Route>
+        <Route
+          path="/projects"
+          element={
+            <>
+              <Projects />
+            </>
+          }
+        ></Route>
+        <Route
+          path="/contacts"
+          element={
+            <>
+              <Contact />
+            </>
+          }
+        ></Route>
       </Routes>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,40 +1,104 @@
 import "./ContactForm.css";
 import Button from "../Button/Button";
+import { useEffect, useState } from "react";
 
 function ContactForm() {
+  const [isFormValid, setIsFormValid] = useState(false);
+  const [formSubmitLoading, setFormSubmitLoading ] = useState(false)
+  const [formSubmitted, setformSubmitted] = useState({})
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (isFormValid) {
+      setFormSubmitLoading(true)
+      try{
+        const response = await fetch('https://api.web3forms.com/submit',{
+          method:'POST',
+          headers:{
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({...formData, access_key: "d3dc1147-e00e-4d8c-9f1e-74299924304f"})
+        })
+        if(response.ok){
+          setformSubmitted(true)
+        }else{
+          alert('Erro ao enviar!')
+        }
+      }catch(e){
+        alert('Erro: ',e)
+      }finally{
+        setFormSubmitLoading(false)
+      }
+    }
+  };
+
+  useEffect(() => {
+    const isValidEmail = (email) => {
+      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      return emailRegex.test(email);
+    };
+    const isValid =
+      formData.name.trim() &&
+      formData.email.trim() &&
+      isValidEmail(formData.email) &&
+      formData.message.trim();
+
+    setIsFormValid(isValid);
+  }, [formData]);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
   return (
     <div className="contact-form d-flex fd-collumn al-center">
       <h2>We love meeting new people and helping them.</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="d-flex form-group">
-          <input 
-          type="text"
-          className="form-input"
-          name="name"
-          id="name"
-          placeholder="Name *"
+          <input
+            type="text"
+            className="form-input"
+            name="name"
+            id="name"
+            placeholder="Name *"
+            onChange={handleChange}
           />
-          <input 
-          type="email"
-          className="form-input"
-          name="email"
-          id="email"
-          placeholder="Email *"
+          <input
+            type="email"
+            className="form-input"
+            name="email"
+            id="email"
+            placeholder="Email *"
+            onChange={handleChange}
           />
-
         </div>
         <div className="d-flex form-group">
-          <textarea 
-          className="form-input"
-          name="message" 
-          id="message"
-          placeholder="Mensagem *"
-          rows="4"
+          <textarea
+            className="form-input"
+            name="message"
+            id="message"
+            placeholder="Mensagem *"
+            rows="4"
+            onChange={handleChange}
           ></textarea>
         </div>
         <div className="al-center d-flex jc-end form-group">
           <div>
-            <Button type="submit" buttonStyle="secondary" >
+            
+            <Button
+              type="submit"
+              buttonStyle="secondary"
+              disabled={!isFormValid || formSubmitLoading}
+            >
               Enviar
             </Button>
           </div>
